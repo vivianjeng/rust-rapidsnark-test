0dcde7bd3978c83cf4b1e21dc20a2a63ac0eb04b
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-08-14 14:18:05 +0800

    Refactor build script by removing unused C stub generation and updating dependencies. The `Cargo.toml` and `Cargo.lock` files have been modified to streamline the `rust-rapidsnark` dependency, ensuring it points to the latest version from the specified Git repository.

--- build.rs
+++ build.rs
@@ -1,21 +1,4 @@
-use std::{env, fs, path::Path};
-
 fn main() {
     rust_witness::transpile::transpile_wasm("./test-vectors/circom".to_string());
     // witnesscalc_adapter::build_and_link("./test-vectors/circom");
-
-    // Create the stub source file
-    let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
-    let stub_path = Path::new(&out_dir).join("chkstk_stub.c");
-    fs::write(
-        &stub_path,
-        r#"
-        void __chkstk_darwin(void) {}
-        "#,
-    )
-    .unwrap();
-
-    // Compile the stub into a static lib
-    cc::Build::new().file(&stub_path).compile("chkstk_stub");
-    println!("cargo:rustc-link-lib=static=chkstk_stub");
 }
